62249e3f0ac67d4cac21df71c9639eaeaddef194
Tramio <tramiodev@outlook.fr>
2022-02-02 10:01:41 +0100

    create library, book constructor and addBookToLibrary function

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
