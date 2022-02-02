512160c717d6e2ce15c117e1bb86432727594c23
Tramio <tramiodev@outlook.fr>
2022-02-02 10:03:12 +0100

    create function to render library in dom

--- script.js
+++ script.js
@@ -10,3 +10,60 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+const DOM = (() => {
+  const renderLibrary = () => {
+    myLibrary.forEach(bookObject => {
+      renderBook(bookObject);
+    });
+  }
+  const renderBook = (bookObject) => {
+    const bookElement = createBook(bookObject);
+    const libraryElement = document.getElementById("library");
+    libraryElement.appendChild(bookElement);
+  }
+  const createBook = (book) => {
+    const div = document.createElement("div");
+    div.classList.add("book-card");
+    div.appendChild(createTitle(book));
+    div.appendChild(createAuthor(book));
+    div.appendChild(createPages(book));
+    div.appendChild(createReadingStatus(book));
+    return div;
+  }
+  const createTitle = (book) => {
+    const element = document.createElement("h1");
+    element.textContent = book.title;
+    return element;
+  }
+  const createAuthor = (book) => {
+    const element = document.createElement("h2");
+    element.textContent = book.author;
+    return element;
+  }
+  const createPages = (book) => {
+    const element = document.createElement("p");
+    element.textContent = book.pages;
+    return element;
+  }
+  const createReadingStatus = (book) => {
+    const element = document.createElement("p");
+    let readingStatus;
+    book.isRead ? readingStatus = "Read" : readingStatus = "Not read yet";
+    element.textContent = readingStatus;
+    return element;
+  }
+  return {
+    renderLibrary,
+  }
+})();
+
+function createSample() {
+  const lotr = new Book("lotr", "tolkien", 418, true);
+  const hp = new Book("hp", "jk rowling", 435, false);
+  addBookToLibrary(lotr);
+  addBookToLibrary(hp);
+}
+
+createSample();
+DOM.renderLibrary();
